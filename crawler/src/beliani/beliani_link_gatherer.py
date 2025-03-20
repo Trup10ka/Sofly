b9a86a6ce2fab2c82a6c07559284949150b9b57b
Trup10ka <lukys3036@gmail.com>
2025-03-20 23:30:47 +0100

    Init beliani link gatherer

---
+++ crawler/src/beliani/beliani_link_gatherer.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+
+def beliani_gather_links() -> int:
+
+    path_to_html = r"D:\Programing\Html\sofly\crawler\input\pohovky_beliani.htm"
+    # Load the HTML file
+    with open(path_to_html, "r", encoding="utf-8") as file:
+        soup = BeautifulSoup(file, "html.parser")
+
+    # Base URL
+    base_url = "https://www.beliani.cz"
+
+    # Extract links
+    links = []
+    for product in soup.find_all("div", class_="product-teaser"):
+        a_tag = product.find("a", class_="itemBox")
+        if a_tag and a_tag.get("href"):
+            links.append(base_url + a_tag["href"])
+
+    # Save to file
+    with open("links-gathered-beliani.txt", "w", encoding="utf-8") as file:
+        file.write("\n".join(links))
+
+    print(f"Extracted {len(links)} links and saved to links-gathered.txt.")
+
+    return len(links)
