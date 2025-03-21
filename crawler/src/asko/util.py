bd2603438319c815a188863467927df081a8e2ac
Lukáš Friedl <lukys3036@gmail.com>
2025-03-21 20:46:01 +0100

    Implement asko util

---
+++ crawler/src/asko/util.py
@@ -0,0 +1,10 @@
+
+
+
+def map_asko_keys():
+    return {
+        "konstrukce": "cover_material",
+        "potah": "cover_material",
+        "rozměry": "length",
+        "výška sedáku": "sit_height"
+    }
