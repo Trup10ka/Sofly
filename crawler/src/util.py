6373bd4df4ae93b77645221fa3f576ba8750c47e
Trup10ka <lukys3036@gmail.com>
2025-03-21 21:56:16 +0100

    Refactor some methods to be reused in different parsers

--- crawler/src/util.py
+++ crawler/src/util.py
@@ -1,7 +1,27 @@
+import csv
 
-ASKO_DESCRIPTION_KEYS = ("konstrukce", "potah", "rozměry", "výška sedáku")
+ASKO_DESCRIPTION_KEYS = ("konstrukce", "potah", "rozměry (š x v x h)", "výška sedáku")
 VENETI_DESCRIPTION_KEYS = ("konstrukce", "potah", "rozměry", "výška sedáku")
 BELIANI_DESCRIPTION_KEYS = ("konstrukce", "potah", "rozměry", "výška sedáku")
+
+def append_parsed_data(all_sofas_parsed_links: list) -> None:
+    with open("sofa-set.csv", "a", newline='', encoding="utf-8") as csvfile:
+        fieldnames = all_sofas_parsed_links[0].keys() if all_sofas_parsed_links else []
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for data in all_sofas_parsed_links:
+            writer.writerow(data)
+
+def finalize_parsed_dict(all_sofas_parsed_links: list, data_dict: dict) -> bool:
+    if len(data_dict) == 0:
+        return False
+
+    default_data = init_data_map()
+    if any(data_dict[key] == default_data[key] for key in data_dict):
+        data_dict["ERROR"] = 1
+    data_dict["ERROR"] = 0
+    all_sofas_parsed_links.append(data_dict)
+    return True
 
 def init_data_map() -> dict:
     return {
