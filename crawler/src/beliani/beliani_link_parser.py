f501768d4b621bb800942ec9087197b781e494ac
Trup10ka <lukys3036@gmail.com>
2025-03-22 02:10:30 +0100

    Make beliani_link_parser use selenium

--- crawler/src/beliani/beliani_link_parser.py
+++ crawler/src/beliani/beliani_link_parser.py
@@ -1,7 +1,10 @@
-import requests
-from bs4 import BeautifulSoup
+import time
 
-from src.util import init_data_map, finalize_parsed_dict, append_parsed_data, get_soup_parser, BELIANI_DESCRIPTION_KEYS
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+
+from src.util import init_data_map, finalize_parsed_dict, append_parsed_data, get_soup_parser, BELIANI_DESCRIPTION_KEYS, \
+    get_selenium_parser, map_key
 
 
 def map_material(material: str) -> str:
@@ -15,9 +18,9 @@ def map_material(material: str) -> str:
 
 def map_additional_material(material: str) -> dict:
     materials = {"contains_metal": 0, "contains_hardwood": 0}
-    if material in ["kov", "kovová", "kovové"]:
+    if any(substring in material for substring in ["kov", "kovová", "kovové", "železo", "ocel", "titan", "hliník", "nerez"]):
         materials["contains_metal"] = 1
-    elif material in ["dřevo", "masiv", "dřevo masiv"]:
+    elif any(substring in material for substring in ["dřevo", "masiv", "dřevo masiv"]):
         materials["contains_hardwood"] = 1
 
     return materials
@@ -36,62 +39,74 @@ def process_key_value(key: str, value: str, data_dict: dict):
         data_dict.update(additional_material)
 
     elif key in ["šířka", "výška", "hloubka"]:
-        data_dict[key] = int(value.replace("cm", "").strip())
+        mapped_key = map_key(key)
+        data_dict[mapped_key] = int(value.replace("cm", "").strip())
 
     elif key == "výška sedáku":
         data_dict["sit_height"] = int(value.replace("cm", "").strip())
 
 
-def parse_all_other_beliani_parameters(soup: BeautifulSoup, data_dict: dict):
-    description_block = soup.find('div', class_='offerDescriptionBlock')
-    if not description_block:
-        return
-
-    all_elements = description_block.find_all('div', class_='list_elem')
-    for element in all_elements:
-        key = element.find('b').text.replace(":", "").strip()
-        value = element.find('span').text.lower().strip()
-
-        if not key or not value:
-            continue
-
-        if key not in BELIANI_DESCRIPTION_KEYS:
-            continue
-
-        process_key_value(key, value, data_dict)
-
-
-def parse_beliani_link(page_link: str) -> dict:
+def parse_all_other_beliani_parameters(selenium_driver: WebDriver, data_dict: dict):
     try:
-        soup = get_soup_parser(page_link)
+        description_block = selenium_driver.find_elements(By.CLASS_NAME, 'offerDescriptionBlock')
+        description_block_one = description_block[1].text
+        description_block_two = description_block[2].text
+        description_block = [line for line in (description_block_one + description_block_two).split("\n") if ":" in line]
 
-        main_price = soup.find('span', class_='price_text')
+        for description in description_block:
+            key, value = description.split(":", 1)
+            key = key.strip().lower()
+            value = value.strip().lower()
+            process_key_value(key, value, data_dict)
+
+        if not description_block:
+            return
+
+
+    except Exception as e:
+        print(f"Error while parsing parameters: {e}")
+
+
+def parse_beliani_link(page_link: str, selenium_driver: WebDriver) -> dict:
+    try:
+        selenium_driver.get(page_link)
+
+        time.sleep(3)
+
+        main_price = selenium_driver.find_element(By.CLASS_NAME, 'price_text')
         price_text = main_price.text if main_price else None
 
-        if price_text is None:
-            raise Exception("Price not found")
-
         data_dict = init_data_map()
+
+        if price_text is None:
+            data_dict["ERROR"] = "Price not found"
+            return data_dict
+
         data_dict["price"] = parse_beliani_price(price_text)
 
-        parse_all_other_beliani_parameters(soup, data_dict)
+        parse_all_other_beliani_parameters(selenium_driver, data_dict)
 
         return data_dict
 
-    except requests.RequestException as e:
-        print(f"Failed to fetch {page_link}: {e}")
+    except Exception as e:
+        print(f"Failed to parse {page_link}: {e}")
         return {}
 
 
-def  parse_beliani_links() -> int:
+def parse_beliani_links() -> int:
     total_parsed_links = 0
     all_sofas_parsed_links = []
+    selenium_driver = get_selenium_parser("https://www.beliani.cz")
     with open("links-gathered-beliani.txt", "r", encoding="utf-8") as f:
         links = f.readlines()
 
         for link in links:
             print(f"Parsing link: {link}")
-            parsed_data = parse_beliani_link(link.strip())
+            parsed_data = parse_beliani_link(link.strip(), selenium_driver)
+
+            if parsed_data.get("ERROR"):
+                all_sofas_parsed_links.append(parsed_data)
+                continue
 
             if not finalize_parsed_dict(all_sofas_parsed_links, parsed_data):
                 print(f"Failed to parse link: {link}")
