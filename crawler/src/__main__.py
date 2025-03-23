796381e31e65b3746ed17afcc728f90def4ab110
Trup10ka <lukys3036@gmail.com>
2025-03-23 22:45:11 +0100

    Sync __main__ across workspaces

--- crawler/src/__main__.py
+++ crawler/src/__main__.py
@@ -1,15 +1,18 @@
 from src.asko.asko_link_gatherer import asko_gather_links
-from src.asko.asko_link_parser import parse_asko_links, parse_link
 from src.beliani.beliani_link_gatherer import beliani_gather_links
+from src.beliani.beliani_link_parser import parse_beliani_links
 from src.veneti.veneti_link_gatherer import veneti_gather_links
+from src.veneti.veneti_link_parser import parse_veneti_links
 
 
 def gather_all_links() -> int:
     return asko_gather_links() + veneti_gather_links() + beliani_gather_links()
 
 def parse_all_links() -> int:
-    return parse_asko_links()
+    # return parse_asko_links() + parse_beliani_links()
     # return parse_asko_links()
+    # return parse_beliani_links()
+    return parse_veneti_links()
 
 if __name__ == '__main__':
     # total_links = gather_all_links()
