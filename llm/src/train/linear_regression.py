af07e97a3c302d7663a92e5089d94c5e8cfe983a
Trup10ka <lukys3036@gmail.com>
2025-03-24 21:48:42 +0100

    Implement linear regression

---
+++ llm/src/train/linear_regression.py
@@ -0,0 +1,34 @@
+import time
+
+from pandas import DataFrame
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+
+def train_linear_regression(sofa_frame: DataFrame):
+
+    x = sofa_frame.iloc[:, :-1]
+    y = sofa_frame.iloc[:, -1]
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state=42)
+
+    scaler = StandardScaler()
+    x_train_scaled = scaler.fit_transform(x_train)
+    x_test_scaled = scaler.transform(x_test)
+
+    model = LinearRegression()
+
+    time_taken_start = time.time()
+    model.fit(x_train_scaled, y_train)
+    print(str(time.time() - time_taken_start) + " seconds")
+
+    y_pred = model.predict(x_test_scaled)
+
+    mse = mean_squared_error(y_test, y_pred)
+    mae = mean_absolute_error(y_test, y_pred)
+    print(f"LINEAR REGRESSION | Mean Squared Error: {mse}")
+    print(f"LINEAR REGRESSION | Mean Absolute Error: {mae}")
+
+    return model
