6e94b738cef4d5d102a61e8d520ba20e4584cc0c
Lukáš Friedl <lukys3036@gmail.com>
2025-03-30 21:12:13 +0200

    Add scripts to run crawler

---
+++ run-crawler.bash
@@ -0,0 +1,4 @@
+source ./.venv/bin/activate
+
+poetry run python3 -m crawler.src.__main__ \
+    --log-level info
---
+++ run-crawler.ps1
@@ -0,0 +1,4 @@
+./.venv/Scripts/activate.ps1
+
+poetry run python3 -m crawler.src.__main__ \
+    --log-level info
