78b2c2f15db867faadf00c4dafc1ca3ddc292420
Trup10ka <lukys3036@gmail.com>
2025-04-01 23:14:35 +0200

    Implement server staring with config values

--- run.py
+++ run.py
@@ -1,9 +1,13 @@
+from src.config.pyhocon_config_loader import PyhoconConfigLoader
 from src.sofly_server import SoflyServer
 
 def main():
+
+    config = PyhoconConfigLoader("config.conf").load_config()
+
     server = (SoflyServer.builder()
-              .set_host('0.0.0.0')
-              .set_port(5525)
+              .set_host(config.server.host)
+              .set_port(config.server.port)
               .build())
 
     if server is None:
