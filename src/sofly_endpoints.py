40054dd424092da43c26087d801ff3d79a4cf7fc
Lukáš Friedl <lukys3036@gmail.com>
2025-04-02 18:48:00 +0200

    Update initializing of all endpoints - arguments type mismatch

--- src/sofly_endpoints.py
+++ src/sofly_endpoints.py
@@ -1,15 +1,16 @@
 from flask import Flask, Blueprint
 from src.endpoints import init_html_endpoints, init_api_endpoints
+from src.sofly_server import SoflyServer
 
 
-def init_endpoints(app: Flask):
+def init_endpoints(app: SoflyServer):
     api_endpoint = Blueprint('api_endpoint', __name__, url_prefix='/api')
 
-    init_api_endpoints(api_endpoint)
+    init_api_endpoints(api_endpoint, app.jwt_service, app.db_client.user_service)
 
-    init_html_endpoints(app)
+    init_html_endpoints(app.flask_app)
 
-    register_all_blueprints(app, api_endpoint)
+    register_all_blueprints(app.flask_app, api_endpoint)
 
 
 
