230eaac96dbeae56e18e4addd064a2a89c4ebb3e
Lukáš Friedl <lukys3036@gmail.com>
2025-04-03 15:53:04 +0200

    Rename 'user_id' to 'for_username'

--- src/data/insurance_dto.py
+++ src/data/insurance_dto.py
@@ -2,10 +2,10 @@ from typing import Literal
 
 class InsuranceDTO:
 
-    def __init__(self, user_id: int, insurance_id: int, insurance_type: str, cost_per_month: float,
+    def __init__(self, for_username: int, insurance_id: int, insurance_type: str, cost_per_month: float,
                  status: Literal['active', 'inactive', 'pending'],
                  start_date: str = None, end_date: str = None, ):
-        self.user_id = user_id
+        self.for_username = for_username
         self.insurance_id = insurance_id
         self.insurance_type = insurance_type
         self.cost_per_month = cost_per_month
@@ -14,6 +14,6 @@ class InsuranceDTO:
         self.end_date = end_date
 
     def __str__(self):
-        return (f"InsuranceDTO(user_id={self.user_id}, insurance_id={self.insurance_id}, "
+        return (f"InsuranceDTO(user_id={self.for_username}, insurance_id={self.insurance_id}, "
                 f"insurance_type={self.insurance_type}, cost_per_month={self.cost_per_month},"
                 f" status={self.status}, start_date={self.start_date}, end_date={self.end_date})")
