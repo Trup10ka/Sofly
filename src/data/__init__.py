1e4a2f44581bec2527975136a46af1958d233d3b
Lukáš Friedl <lukys3036@gmail.com>
2025-04-03 16:24:32 +0200

    Expose Insurance and InsuranceDTO from data module

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,4 +1,6 @@
 from .user_dto import UserDTO
 from .user import User
+from .insurance_dto import InsuranceDTO
+from .insurance import Insurance
 
-_all__ = ["UserDTO", "User"]
+_all__ = ["UserDTO", "User", "InsuranceDTO", "Insurance"]
