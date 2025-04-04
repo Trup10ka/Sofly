bb31a6302a88a51f9e832ad2f997beb330ad0273
Trup10ka <lukys3036@gmail.com>
2025-04-05 01:20:22 +0200

    Add logging remote address of request

--- src/endpoints/html_endpoints.py
+++ src/endpoints/html_endpoints.py
@@ -10,12 +10,12 @@ def init_html_endpoints(app: Flask, jwt_service: JWTService):
 
     @app.route('/')
     def serve_index():
-        logger.info("Request to '/' from origin: ")
+        logger.info(f"Request to '/' from origin: {request.remote_addr}")
         return send_from_directory(HTML_DIR, 'index.html')
 
     @app.route('/register')
     def server_register():
-        logger.info("Request to '/register' from origin: ")
+        logger.info(f"Request to '/register' from origin: {request.remote_addr}")
         return send_from_directory(HTML_DIR, 'register.html')
 
     @app.route('/dashboard')
@@ -23,7 +23,7 @@ def init_html_endpoints(app: Flask, jwt_service: JWTService):
         if not is_authenticated(jwt_service):
             return redirect("/", code=302)
 
-        logger.info("Request to '/dashboard' from origin: ")
+        logger.info(f"Request to '/dashboard' from origin: {request.remote_addr}")
         return send_from_directory(HTML_DIR, 'dashboard.html')
 
     @app.route('/insurance')
@@ -31,7 +31,7 @@ def init_html_endpoints(app: Flask, jwt_service: JWTService):
         if not is_authenticated(jwt_service):
             return redirect("/", code=302)
 
-        logger.info("Request to '/insured-event' from origin: ")
+        logger.info(f"Request to '/insured-event' from origin: {request.remote_addr}")
         return send_from_directory(HTML_DIR, 'insurance.html')
 
     @app.route('/report')
@@ -39,7 +39,7 @@ def init_html_endpoints(app: Flask, jwt_service: JWTService):
         if not is_authenticated(jwt_service):
             return redirect("/", code=302)
 
-        logger.info("Request to '/insured-event' from origin: ")
+        logger.info(f"Request to '/insured-event' from remote address: {request.remote_addr}")
         return send_from_directory(HTML_DIR, 'report-event.html')
 
 def is_authenticated(jwt_service: JWTService) -> bool:
