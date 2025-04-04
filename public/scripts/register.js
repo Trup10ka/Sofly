36246d9e37a0d6454e7a2500ff91a9d62a82db02
Lukáš Friedl <lukys3036@gmail.com>
2025-04-04 10:59:45 +0200

    Implement register page

---
+++ public/scripts/register.js
@@ -0,0 +1,35 @@
+
+registerButton = document.getElementById("register-button")
+
+registerButton.addEventListener("click", async function (event)
+    {
+        event.preventDefault()
+        register()
+    }
+)
+
+async function register() {
+    const username = document.getElementById("username").value
+    const password = document.getElementById("password").value
+    const email = document.getElementById("email").value
+
+    const response = await fetch('/api/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(
+            {
+                username: username,
+                password: password,
+                email: email
+            }
+        )
+    });
+
+    if (response.ok) {
+        window.location.href = '/dashboard';
+    } else {
+        alert('Login failed');
+    }
+}
